c143520aca7dc6ff7a6c660e2b04119800461140
Rezki Ramadhan <rezki@dev.netkromsolution.com>
2021-02-09 10:19:58 +0700

    bm check api

--- app/Http/Controllers/CheckBmrController.php
+++ app/Http/Controllers/CheckBmrController.php
@@ -29,14 +29,14 @@ class CheckBmController extends Controller
             'tinggi_badan'   => 'required',
             'umur'   => 'required',
             'gender'   => 'required',
-            'type'   => 'required',
+            'type_bmr'   => 'required',
         ],
             [
                 'nama.required' => 'Nama harus diisi !',
                 'tinggi_badan.required' => 'Tinggi badan harus diisi !',
                 'berat_badan.required' => 'Berat badan harus diisi !',
                 'umur.required' => 'Umur harus diisi !',
-                'type.required' => 'Type harus diisi !',
+                'type_bmr.required' => 'type_bmr harus diisi !',
             ]
         );
 
@@ -49,33 +49,81 @@ class CheckBmController extends Controller
 
         } else {
 
+            // Sangat jarang olahraga, kalikan BMR dengan 1,2
+            // Jarang olahraga (1-3 hari/ minggu), kalikan BMR dengan 1,375
+            // Normal olahraga (3-5 hari/ minggu), kalikan BMR dengan 1,55
+            // Sering olahraga (6-7 hari/ minggu), kalikan BMR dengan 1,725
+            // Sangat sering olahraga (setiap hari bisa dua kali dalam sehari), kalikan BMR dengan 1,9
+
             $bb = $request->input('berat_badan');
             $tb = $request->input('tinggi_badan') / 100;
+            $gender = $request->input('gender');
+            $umur = $request->input('umur');
+            $type = $request->input('type_bmr');
 
-            // $Bmr = $bb / ($tb*$tb);
-            // if($Bmr < 15.5) {
-            //     $message = "Anda mengalami anoreksia";
-            //     $kategori = 1;
-            // }elseif ($Bmr < 18.5 ) {
-            //     $message = "Anda mengalami kekurangan gizi";
-            //     $kategori = 1;
-            // }elseif ($Bmr < 25 ) {
-            //     $message = "Anda memiliki berat badan normal";
-            //     $kategori = 2;
-            // }elseif ($Bmr < 30 ) {
-            //     $message = "Anda memiliki overweight";
-            //     $kategori = 3;
-            // }elseif ($Bmr < 35 ) {
-            //     $message = "Anda mengalami Obesitas Level 1";
-            //     $kategori = 4;
-            // }elseif ($Bmr < 40 ) {
-            //     $message = "Anda mengalami Obesitas Level 2";
-            //     $kategori = 4;
-            // }else {
-            //     $message = "Anda mengalami Obesitas Akut";
-            //     $kategori = 4;
-            // }
-            // dd($Bmr);
+            if($type == 'Harris Benedict'){
+                if($gender == 'L'){
+                    $bmr =  66 + (13.7 * $bb) + (5 * $tb) - (6.78 * $umur);
+                    if($bmr <= 1.2){
+                        $data_bmr = array(
+                                        'bmr' => $bmr,
+                                        'deskripsi' => 'Sangat jarang olahraga',
+                                    );
+                    }else if($bmr <= 1.375){
+                        $data_bmr = array(
+                                        'bmr' => $bmr,
+                                        'deskripsi' => 'Jarang olahraga (1-3 hari/ minggu)',
+                                    );
+                    }else if($bmr <= 1.55){
+                        $data_bmr = array(
+                                        'bmr' => $bmr,
+                                        'deskripsi' => 'Normal olahraga (3-5 hari/ minggu)',
+                                    );
+                    }else if($bmr <= 1.725){
+                        $data_bmr = array(
+                                        'bmr' => $bmr,
+                                        'Sering olahraga (6-7 hari/ minggu)',
+                                    );
+                    }else if($bmr <= 1.9){
+                        $data_bmr = array(
+                                        'bmr' => $bmr,
+                                        'Sangat sering olahraga (setiap hari bisa dua kali dalam sehari)',
+                                    );
+                    }
+                }else if($gender == 'P'){
+                    $bmr = 655 + (9.6 * $bb) + (1.8 * $tb) - (4.7 * $umur);
+                    if($bmr <= 1.2){
+                        $data_bmr = array(
+                                        'bmr' => $bmr,
+                                        'deskripsi' => 'Sangat jarang olahraga',
+                                    );
+                    }else if($bmr <= 1.375){
+                        $data_bmr = array(
+                                        'bmr' => $bmr,
+                                        'deskripsi' => 'Jarang olahraga (1-3 hari/ minggu)',
+                                    );
+                    }else if($bmr <= 1.55){
+                        $data_bmr = array(
+                                        'bmr' => $bmr,
+                                        'deskripsi' => 'Normal olahraga (3-5 hari/ minggu)',
+                                    );
+                    }else if($bmr <= 1.725){
+                        $data_bmr = array(
+                                        'bmr' => $bmr,
+                                        'deskripsi' => 'Sering olahraga (6-7 hari/ minggu)',
+                                    );
+                    }else if($bmr <= 1.9){
+                        $data_bmr = array(
+                                        'bmr' => $bmr,
+                                        'deskripsi' => 'Sangat sering olahraga (setiap hari bisa dua kali dalam sehari)',
+                                    );
+                    }
+                }
+            }else if($type == ''){
+
+            }else{
+
+            }
             
             $CheckBmr = CheckBmr::create([
                 'nama'     => $request->input('nama'),
@@ -83,7 +131,7 @@ class CheckBmController extends Controller
                 'berat_badan'   => $request->input('berat_badan'),
                 'umur'   => $request->input('umur'),
                 'gender'   => $request->input('gender'),
-                'type'   => $type 
+                'type_bmr'   => $request->input('type_bmr')
             ]);
 
 
@@ -97,7 +145,7 @@ class CheckBmController extends Controller
                         'berat_badan'   => $request->berat_badan,
                         'umur'   => $request->umur,
                         'gender'   => $request->gender,
-                        'message' => $message 
+                        'data_bmr' => $data_bmr    
                     ],
                 ], 200);
             } else {
